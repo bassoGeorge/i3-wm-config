a44459807f14e77f32336d6766bf38840267430e
Anish George <anish.george@ignitesol.com>
2017-10-03 09:49:10 +0530

    Move scripts, update calls

---
+++ scripts/monitors_3_mid.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output DP1 --mode 1600x900 --pos 3286x220 --rotate normal --output eDP1 --primary --mode 1366x768 --pos 0x488 --rotate normal --output VIRTUAL1 --off
